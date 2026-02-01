d73d96c1bdc286aabb4e2fe4f2572917b99320a0
MAK <denbor140@gmail.com>
2026-02-01 20:03:07 +0200

    add notehub

---
+++ src/components/App/App.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import SearchBox from "../SearchBox/SearchBox";
+import css from "./App.module.css";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { fetchNotes, type FetchNotesResponse } from "../services/noteService";
+import NoteList from "../NoteList/NoteList";
+import Pagination from "../Pagination/Pagination";
+import Modal from "../Modal/Modal";
+import NoteForm from "../NoteForm/NoteForm";
+import { useDebouncedCallback } from "use-debounce";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+
+const PER_PAGE = 12;
+
+function App() {
+  const [searchText, setSearchText] = useState("");
+  const [page, setPage] = useState(1);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { data, isLoading, isError } = useQuery<FetchNotesResponse>({
+    queryKey: ["notes", searchText, page],
+    queryFn: () => fetchNotes(searchText, page, PER_PAGE),
+    retry: 1,
+    refetchOnWindowFocus: false,
+    placeholderData: keepPreviousData,
+  });
+
+  const debounceSearchText = useDebouncedCallback(setSearchText, 300);
+
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        <SearchBox text={searchText} onSearch={debounceSearchText} />
+        {data && data.totalPages > 1 && (
+          <Pagination
+            totalPages={data.totalPages}
+            page={page}
+            onPageChange={setPage}
+          />
+        )}
+        <button className={css.button} onClick={() => setIsModalOpen(true)}>
+          Create note +
+        </button>
+      </header>
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {isModalOpen && (
+        <Modal onClose={() => setIsModalOpen(false)}>
+          <NoteForm onClose={() => setIsModalOpen(false)} />
+        </Modal>
+      )}
+      {data && data.notes.length > 0 && <NoteList notes={data.notes} />}
+    </div>
+  );
+}
+
+export default App;
